59d03b1ed7d2506e6dcc0689fc5a4c4f01527f49
Stepa <cntgf.2010@yandex.ru>
2022-02-01 14:39:29 +0300

    Refactor Price of Transit (nullable)

--- SpaceZD.DataLayer/Entities/Transit.cs
+++ SpaceZD.DataLayer/Entities/Transit.cs
@@ -5,7 +5,7 @@ public class Transit
     public int Id { get; set; }
     public virtual Station StartStation { get; set; }
     public virtual Station EndStation { get; set; }
-    public decimal Price { get; set; }
+    public decimal? Price { get; set; }
     public bool IsDeleted { get; set; }
     public virtual ICollection<RouteTransit> RouteTransit { get; set; }
 }
