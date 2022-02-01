5007298b9b310f25cca90a982b8ef1c5c64ac210
svyatopes <svyatopes@gmail.com>
2022-02-02 00:49:16 +0300

    added OrderController

---
+++ SpaceZD.API/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceZD.BusinessLayer.Models;
+
+namespace SpaceZD.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetOrders()
+        {
+            return Ok("Must be orders");
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetOrderById(int id)
+        {
+            return Ok("MustBeOrder");
+        }
+
+        [HttpGet("with-tickets/{id}")]
+        public ActionResult GetOrderByIdWithTickets(int id)
+        {
+            return NotFound("Can't find(((((");
+        }
+
+        [HttpPost]
+        public ActionResult AddOrder(OrderModel order)
+        {
+            return StatusCode(StatusCodes.Status201Created, order);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult EditOrder(int id, OrderModel order)
+        {
+            return BadRequest();
+        }
+
+        
+        [HttpDelete("{id}")]
+        public ActionResult DeleteOrder(int id)
+        {
+            return Accepted();
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult RestoreOrder(int id)
+        {
+            return Accepted();
+        }
+    }
+}
