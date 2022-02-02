49f23a6049f14db40e521a361e952927ca4960b9
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-02 15:42:27 +0300

    Added trip station repository

--- SpaceZD.DataLayer/Repositories/TripStationRepository.cs
+++ SpaceZD.DataLayer/Repositories/TripStationRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpaceZD.DataLayer.DbContextes;
+using SpaceZD.DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,42 @@ namespace SpaceZD.DataLayer.Repositories
 {
     internal class TripStationRepository
     {
+        private readonly VeryVeryImportantContext _context;
+        public TripStationRepository() => _context = VeryVeryImportantContext.GetInstance();
+
+        public void Add(TripStation tripStation)
+        {
+            _context.TripStations.Add(tripStation);
+            Save();
+        }
+
+        public TripStation GetEntity(int id)
+        {
+            var entity = _context.TripStations.FirstOrDefault(c => c.Id == id);
+            if (entity == null)
+                throw new Exception($"TripStation c Id = {id} не найден");
+
+            return entity;
+        }
+
+        public IEnumerable<TripStation> GetListEntity() => _context.TripStations.ToList();
+
+
+        public void Update(TripStation tripStation)
+        {
+            var entity = GetEntity(tripStation.Id);
+
+            entity.Station = tripStation.Station;
+            entity.Platform = tripStation.Platform;
+            entity.ArrivalTime = tripStation.ArrivalTime;
+            entity.DepartingTime = tripStation.DepartingTime;
+            entity.Trip = tripStation.Trip;
+            entity.OrdersWithStartStation = tripStation.OrdersWithStartStation;
+            entity.OrdersWithEndStation = tripStation.OrdersWithEndStation;
+
+            Save();
+        }
+
+        public void Save() => _context.SaveChanges();
     }
 }
