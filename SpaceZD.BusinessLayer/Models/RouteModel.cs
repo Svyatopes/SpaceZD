d3b9258b75df9720d37f532e7cc2f48c2615bd56
Aleksey <alexandro361@gmail.com>
2022-02-02 16:04:44 +0300

    add controller,model,repository for route

---
+++ SpaceZD.BusinessLayer/Models/RouteModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SpaceZD.BusinessLayer.Models
+{
+    public class RouteModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        //public List<RouteTransit> Transits { get; set; }
+        public DateTime StartTime { get; set; }
+        //public virtual Station StartStation { get; set; }
+        // public virtual Station EndStation { get; set; }
+        public bool IsDeleted { get; set; }
+        // public virtual ICollection<Trip> Trips { get; set; }
+    }
+}
---
+++ SpaceZD.DataLayer/Repositories/RouteRepository.cs
@@ -0,0 +1,59 @@
+﻿using SpaceZD.DataLayer.DbContextes;
+using SpaceZD.DataLayer.Entities;
+
+namespace SpaceZD.DataLayer.Repositories
+{
+    public class RouteRepository
+    {
+        public Route GetRouteById(int id)
+        {
+            var context = VeryVeryImportantContext.GetInstance();
+            var route = context.Routes.FirstOrDefault(r => r.Id == id);
+
+            return route;
+        }
+
+        public List<Route> GetRoutes()
+        {
+            var context = VeryVeryImportantContext.GetInstance();
+            var routes = context.Routes.ToList();
+
+            return routes;
+        }
+
+        public void AddRoute(Route route)
+        {
+            var context = VeryVeryImportantContext.GetInstance();
+
+            context.Routes.Add(route);
+
+            context.SaveChanges();
+        }
+
+        public void DeleteRoute(int id)
+        {
+            var context = VeryVeryImportantContext.GetInstance();
+            var route = context.Routes.FirstOrDefault(r => r.Id == id);
+
+            route.IsDeleted = true;
+
+            context.SaveChanges();
+        }
+
+        public void UpdateRoute(Route route)
+        {
+            var context = VeryVeryImportantContext.GetInstance();
+            var routeDB = GetRouteById(route.Id);
+
+            routeDB.Code = route.Code;
+            routeDB.Transits = route.Transits;
+            routeDB.StartTime = route.StartTime;
+            routeDB.StartStation = route.StartStation;
+            routeDB.EndStation = route.EndStation;
+            routeDB.IsDeleted = route.IsDeleted;
+            routeDB.Trips = route.Trips;
+
+            context.SaveChanges();
+        }
+    }
+}
