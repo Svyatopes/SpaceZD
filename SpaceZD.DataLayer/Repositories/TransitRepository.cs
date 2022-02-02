07209291c5e27f51239a23eef1c44140df71f789
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-02 15:24:43 +0300

    Added transit repository

--- SpaceZD.DataLayer/Repositories/TransitRepository.cs
+++ SpaceZD.DataLayer/Repositories/TransitRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SpaceZD.DataLayer.DbContextes;
+using SpaceZD.DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace SpaceZD.DataLayer.Repositories
 {
-    internal class TransitRepository
+    public class TransitRepository
     {
+        private readonly VeryVeryImportantContext _context;
+        public TransitRepository() => _context = VeryVeryImportantContext.GetInstance();
+
+        public void Add(Transit transit)
+        {
+            _context.Transits.Add(transit);
+            Save();
+        }
+
+        public Transit GetEntity(int id)
+        {
+            var entity = _context.Transits.FirstOrDefault(c => c.Id == id);
+            if (entity == null)
+                throw new Exception($"Transit c Id = {id} не найден");
+
+            return entity;
+        }
+
+        public IEnumerable<Transit> GetListEntity() => _context.Transits.ToList();
+
+        public void Delete(int id)
+        {
+            GetEntity(id).IsDeleted = true;
+
+            Save();
+        }
+
+        public void Update(Transit transit)
+        {
+            var entity = GetEntity(transit.Id);
+
+            entity.StartStation = transit.StartStation;
+            entity.EndStation = transit.EndStation;
+            entity.Price = transit.Price;
+            entity.IsDeleted = transit.IsDeleted;
+            entity.RouteTransit = transit.RouteTransit;
+
+            Save();
+        }
+
+        public void Save() => _context.SaveChanges();
     }
 }
