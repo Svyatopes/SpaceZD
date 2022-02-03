51273010062dd3825cf7a0569a75312ff3f30c4e
svyatopes <svyatopes@gmail.com>
2022-02-03 22:44:37 +0300

    refactor api models

---
+++ SpaceZD.API/Models/Outputs/TripFullOutputModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceZD.API.Models
+{
+    public class TripFullOutputModel : TripShortOutputModel
+    {
+        public List<TripStationOutputModel> Stations { get; set; }
+    }
+}
--- SpaceZD.API/Models/Outputs/TripStationOutputModel.cs
+++ SpaceZD.API/Models/Outputs/TripStationOutputModel.cs
@@ -3,12 +3,10 @@
     public class TripStationOutputModel
     {
         public int Id { get; set; }
-        public StationOutputModel Station { get; set; }
+        public StationShortOutputModel Station { get; set; }
         public PlatformOutputModel Platform { get; set; }
         public DateTime ArrivalTime { get; set; }
         public DateTime DepartingTime { get; set; }
-        public TripOutputModel Trip { get; set; }
-        public ICollection<OrderOutputModel> OrdersWithStartStation { get; set; }
-        public ICollection<OrderOutputModel> OrdersWithEndStation { get; set; }
+        public TripShortOutputModel Trip { get; set; }
     }
 }
