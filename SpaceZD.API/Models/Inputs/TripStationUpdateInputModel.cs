9dcc4b6a4258d461bcb6def8a3bfb71216a914b1
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-03 22:20:09 +0300

    Edited TripStation

--- SpaceZD.API/Models/Inputs/TripStationUpdateInputModel.cs
+++ SpaceZD.API/Models/Inputs/TripStationUpdateInputModel.cs
@@ -1,11 +1,9 @@
 ﻿namespace SpaceZD.API.Models
 {
-    public class TripStationInputModel
+    public class TripStationUpdateInputModel
     {
-        public int StationId { get; set; }
         public int PlatformId { get; set; }
         public DateTime ArrivalTime { get; set; }
         public DateTime DepartingTime { get; set; }
-        public int TripId { get; set; }
     }
 }
--- SpaceZD.API/Models/Inputs/TripStationUpdateModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpaceZD.API.Models
-{
-    public class TripStationUpdateModel
-    {
-        public int PlatformId { get; set; }
-        public DateTime ArrivalTime { get; set; }
-        public DateTime DepartingTime { get; set; }
-    }
-}
