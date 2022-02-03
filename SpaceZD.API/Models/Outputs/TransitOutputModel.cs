16eaeeec2a3c4473967fffbc7fab32733b7c2971
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-03 22:47:04 +0300

    Remove unn. things from Transit Output model

--- SpaceZD.API/Models/Outputs/TransitOutputModel.cs
+++ SpaceZD.API/Models/Outputs/TransitOutputModel.cs
@@ -3,10 +3,8 @@
     public class TransitOutputModel
     {
         public int Id { get; set; }
-        public StationOutputModel StartStation { get; set; }
-        public StationOutputModel EndStation { get; set; }
+        public StationShortOutputModel StartStation { get; set; }
+        public StationShortOutputModel EndStation { get; set; }
         public decimal? Price { get; set; }
-        public bool IsDeleted { get; set; }
-        public ICollection<RouteTransitOutputModel> RouteTransit { get; set; }
     }
 }
