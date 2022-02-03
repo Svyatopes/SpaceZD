37290c7ef0bff0dbad974fd9095f1cd834975d73
svyatopes <svyatopes@gmail.com>
2022-02-03 23:40:46 +0300

    template automapper for api layer

---
+++ SpaceZD.API/Configuration/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace SpaceZD.API.Configuration
+{
+    public static class APIMapper
+    {
+        private static Mapper _instance;
+
+        public static Mapper GetInstance()
+        {
+            if (_instance == null)
+                InitializeInstance();
+            return _instance;
+        }
+
+        private static void InitializeInstance()
+        {
+
+        }
+    }
+}
