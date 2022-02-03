898f593f30846c12975bbf3e07c030e792475111
Stepa <cntgf.2010@yandex.ru>
2022-02-03 22:41:20 +0300

    Refactor API model

--- SpaceZD.API/Models/Outputs/PlatformMaintenanceOutputModel.cs
+++ SpaceZD.API/Models/Outputs/PlatformMaintenanceOutputModel.cs
@@ -3,7 +3,7 @@ namespace SpaceZD.API.Models;
 public class PlatformMaintenanceOutputModel
 {
     public int Id { get; set; }
-    //public PlatformOutputModel Platform { get; set; }
+    public int PlatformId { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
 }
