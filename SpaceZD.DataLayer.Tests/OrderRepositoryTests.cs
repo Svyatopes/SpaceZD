a3cc80224c914d1283f26b8db1ec9a2a556e6dd0
svyatopes <svyatopes@gmail.com>
2022-02-07 23:54:34 +0300

    Refactor OrderRepository

--- SpaceZD.DataLayer.Tests/OrderRepositoryTests.cs
+++ SpaceZD.DataLayer.Tests/OrderRepositoryTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using SpaceZD.DataLayer.DbContextes;
 using SpaceZD.DataLayer.Entities;
 using SpaceZD.DataLayer.Repositories;
+using SpaceZD.DataLayer.Tests.TestCaseSources;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,213 +25,116 @@ public class OrderRepositoryTests
         _context.Database.EnsureCreated();
 
         _repository = new OrderRepository(_context);
-    }
 
+        //seed
+        var orders = OrderRepositoryMocks.GetOrders();
+        _context.Orders.AddRange(orders);
+        _context.SaveChanges();
+    }
 
 
 
     [Test]
     public void GetByIdTest()
     {
-        //arrange
-        var orderToAdd = GetTestOrder();
+        //given
+        var order = _context.Orders.First();
 
-        _context.Orders.Add(orderToAdd);
-        _context.SaveChanges();
-        var idAddedOrder = orderToAdd.Id;
+        //when
+        var receivedOrder = _repository.GetById(order.Id);
 
-        //act
-        var receivedOrder = _repository.GetById(idAddedOrder);
-
-        //assert
-        Assert.IsNotNull(receivedOrder);
-        Assert.IsFalse(receivedOrder!.IsDeleted);
-        AssertTestOrder(receivedOrder);
+        //then
+        Assert.AreEqual(order, receivedOrder);
     }
 
-
-
-    [Test]
-    public void GetListTest()
+    [TestCase(true)]
+    [TestCase(false)]
+    public void GetListAllIncludedTest(bool allIncluded)
     {
-        //arrange
-        var orderToAdd = GetTestOrder();
-        var secondOrderToAdd = GetTestOrder();
-        var thirdOrderToAdd = GetTestOrder();
-        thirdOrderToAdd.IsDeleted = true;
+        //given
+        var orders = _context.Orders.Where(o => !o.IsDeleted || allIncluded);
 
-        _context.Orders.Add(orderToAdd);
-        _context.Orders.Add(secondOrderToAdd);
-        _context.Orders.Add(thirdOrderToAdd);
-        _context.SaveChanges();
+        //when
+        var ordersInDB = (List<Order>)_repository.GetList(allIncluded);
 
-        //act
-        var orders = (List<Order>)_repository.GetList();
-
-
-        //assert
-
-        Assert.IsNotNull(orders);
-        Assert.AreEqual(2, orders.Count);
-
-        var orderToCheck = orders[0];
-        Assert.IsNotNull(orderToCheck);
-        Assert.IsFalse(orderToCheck.IsDeleted);
-        AssertTestOrder(orderToCheck);
-
-    }
-
-
-    [Test]
-    public void GetListAllIncludedTest()
-    {
-        //arrange
-        var orderToAdd = GetTestOrder();
-        var secondOrderToAdd = GetTestOrder();
-        var thirdOrderToAdd = GetTestOrder();
-        thirdOrderToAdd.IsDeleted = true;
-
-        _context.Orders.Add(orderToAdd);
-        _context.Orders.Add(secondOrderToAdd);
-        _context.Orders.Add(thirdOrderToAdd);
-        _context.SaveChanges();
-
-        //act
-        var orders = (List<Order>)_repository.GetList(true);
-
-
-        //assert
-
-        Assert.IsNotNull(orders);
-        Assert.AreEqual(3, orders.Count);
-
-        var orderToCheck = orders[2];
-        Assert.IsNotNull(orderToCheck);
-        Assert.IsTrue(orderToCheck.IsDeleted);
-        AssertTestOrder(orderToCheck);
+        //then
+        Assert.AreEqual(orders, ordersInDB);
 
     }
 
     [Test]
     public void AddTest()
     {
-        //arrange
-        var orderToAdd = GetTestOrder();
+        //given
+        var orderToAdd = OrderRepositoryMocks.GetOrder();
 
-        //act 
+        //when 
         int id = _repository.Add(orderToAdd);
 
-        //assert
+        //then
         var createdOrder = _context.Orders.FirstOrDefault(o => o.Id == id);
 
-        AssertTestOrder(createdOrder);
+        Assert.AreEqual(orderToAdd, createdOrder);
     }
-
+    
     [Test]
     public void UpdateEntityTest()
     {
-        //arrange
-        var orderToAdd = GetTestOrder();
-        _context.Orders.Add(orderToAdd);
-        _context.SaveChanges();
+        //given
+        var order = OrderRepositoryMocks.GetOrders()[0];
 
-        var orderToEdit = GetTestOrder();
-        orderToEdit.Id = orderToAdd.Id;
+        order.Id = _context.Orders.First().Id;
+        
 
-        orderToEdit.Trip = new Trip() { Route = new Route() { Code = "231B" } };
-        orderToEdit.StartStation = new TripStation()
+        order.Trip = new Trip() { Route = new Route() { Code = "231B" } };
+        order.StartStation = new TripStation()
         {
             Station = new Station() { Name = "Tomsk" }
         };
-        orderToEdit.EndStation = new TripStation()
+        order.EndStation = new TripStation()
         {
             Station = new Station() { Name = "Bolotnoe" }
         };
-        orderToEdit.User = new User()
+
+        var originalUser = order.User;
+
+        order.User = new User()
         {
             Name = "Enot",
             Role = Enums.Role.User
         };
 
-        //act 
-        bool edited = _repository.Update(orderToEdit);
+        //when 
+        bool edited = _repository.Update(order);
 
-        //assert
-        var updatedOrder = _context.Orders.FirstOrDefault(o => o.Id == orderToEdit.Id);
+        //then
+        var updatedOrder = _context.Orders.FirstOrDefault(o => o.Id == order.Id);
 
-        Assert.IsNotNull(updatedOrder);
-        Assert.IsNotNull(updatedOrder!.StartStation);
-        Assert.IsNotNull(updatedOrder.StartStation.Station);
-        Assert.AreEqual("Tomsk", updatedOrder.StartStation.Station.Name);
-        Assert.IsNotNull(updatedOrder.EndStation);
-        Assert.IsNotNull(updatedOrder.EndStation.Station);
+        Assert.AreEqual("Tomsk", updatedOrder!.StartStation.Station.Name);
         Assert.AreEqual("Bolotnoe", updatedOrder.EndStation.Station.Name);
-        Assert.IsNotNull(updatedOrder.Trip);
-        Assert.IsNotNull(updatedOrder.Trip.Route);
         Assert.AreEqual("231B", updatedOrder.Trip.Route.Code);
-        Assert.IsNotNull(updatedOrder.User);
-        //ORIGINAL USER NOT CHANGED, AND IT'S FINE 
-        Assert.AreEqual("Zagit", updatedOrder.User.Name);
-        Assert.AreEqual(Enums.Role.StationManager, updatedOrder.User.Role);
+        Assert.AreEqual(originalUser.Name, updatedOrder.User.Name);
     }
 
     [TestCase(true)]
     [TestCase(false)]
     public void UpdateIsDeletedTest(bool isDeleted)
     {
-        //arrange
-        var orderToEdit = GetTestOrder();
+        //given
+        var orderToEdit = OrderRepositoryMocks.GetOrder();
         orderToEdit.IsDeleted = !isDeleted;
         _context.Orders.Add(orderToEdit);
         _context.SaveChanges();
 
-        //act 
+        //when 
         bool edited = _repository.Update(orderToEdit.Id, isDeleted);
 
-        //assert
+        //then
         
         var updatedOrder = _context.Orders.FirstOrDefault(o => o.Id == orderToEdit.Id);
 
         Assert.IsTrue(edited);
-        Assert.IsNotNull(updatedOrder);
         Assert.AreEqual(isDeleted, updatedOrder!.IsDeleted);
-        AssertTestOrder(updatedOrder);
-    }
-
-    private Order GetTestOrder() => new Order()
-    {
-        StartStation = new TripStation()
-        {
-            Station = new Station() { Name = "Novosibirsk" }
-        },
-        EndStation = new TripStation()
-        {
-            Station = new Station() { Name = "Sheregesh" }
-        },
-        Trip = new Trip() { Route = new Route() { Code = "231A" } },
-        User = new User()
-        {
-            Name = "Zagit",
-            Login = "Sleep2000",
-            PasswordHash = "SDJKLSAJDLJASLDJASLDKJASD",
-            Role = Enums.Role.StationManager
-        }
-    };
-
-    private void AssertTestOrder(Order order)
-    {
-        Assert.IsNotNull(order);
-        Assert.IsNotNull(order!.StartStation);
-        Assert.IsNotNull(order.StartStation.Station);
-        Assert.AreEqual("Novosibirsk", order.StartStation.Station.Name);
-        Assert.IsNotNull(order.EndStation);
-        Assert.IsNotNull(order.EndStation.Station);
-        Assert.AreEqual("Sheregesh", order.EndStation.Station.Name);
-        Assert.IsNotNull(order.Trip);
-        Assert.IsNotNull(order.Trip.Route);
-        Assert.AreEqual("231A", order.Trip.Route.Code);
-        Assert.IsNotNull(order.User);
-        Assert.AreEqual("Zagit", order.User.Name);
-        Assert.AreEqual(Enums.Role.StationManager, order.User.Role);
-    }
+        Assert.AreEqual(orderToEdit, updatedOrder);
+    } 
 }
---
+++ SpaceZD.DataLayer.Tests/TestMocks/OrderRepositoryMocks.cs
@@ -0,0 +1,87 @@
+﻿using SpaceZD.DataLayer.Entities;
+using System.Collections.Generic;
+
+namespace SpaceZD.DataLayer.Tests.TestCaseSources
+{
+    public class OrderRepositoryMocks
+    {
+        public static List<Order> GetOrders() => new List<Order>
+        {
+            new Order()
+            {
+                StartStation = new TripStation()
+                {
+                    Station = new Station() { Name = "Novosibirsk" }
+                },
+                EndStation = new TripStation()
+                {
+                    Station = new Station() { Name = "Sheregesh" }
+                },
+                Trip = new Trip() { Route = new Route() { Code = "231A" } },
+                User = new User()
+                {
+                    Name = "Zagit",
+                    Login = "Sleep2000",
+                    PasswordHash = "SDJKLSAJDLJASLDJASLDKJASD",
+                    Role = Enums.Role.StationManager
+                },
+                IsDeleted = true
+            },
+
+            new Order()
+            {
+                StartStation = new TripStation()
+                {
+                    Station = new Station() { Name = "Novosibirsk" }
+                },
+                Trip = new Trip() { Route = new Route() { Code = "231A" } },
+                User = new User()
+                {
+                    Name = "Zagit",
+                    Login = "Sleep2000",
+                    PasswordHash = "SDJKLSAJDLJASLDJASLDKJASD",
+                    Role = Enums.Role.StationManager
+                }
+            },
+
+            new Order()
+            {
+                EndStation = new TripStation()
+                {
+                    Station = new Station() { Name = "Sheregesh" }
+                },
+                User = new User()
+                {
+                    Name = "Jora",
+                    Login = "Sleep2001",
+                    PasswordHash = "SDSDSD",
+                    Role = Enums.Role.StationManager
+                }
+            },
+        };
+
+
+        public static Order GetOrder() => new Order()
+        {
+            StartStation = new TripStation()
+            {
+                Station = new Station() { Name = "Novosibirsk" }
+            },
+            EndStation = new TripStation()
+            {
+                Station = new Station() { Name = "Sheregesh" }
+            },
+            Trip = new Trip() { Route = new Route() { Code = "231A" } },
+            User = new User()
+            {
+                Name = "Zagit",
+                Login = "Sleep2000",
+                PasswordHash = "SDJKLSAJDLJASLDJASLDKJASD",
+                Role = Enums.Role.StationManager
+            },
+            IsDeleted = true
+        };
+        
+
+    }
+}
