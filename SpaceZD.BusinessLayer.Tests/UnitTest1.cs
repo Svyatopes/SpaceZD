d9295788050baae5c9327ca4812f26d888f9bd39
svyatopes <svyatopes@gmail.com>
2022-02-08 00:31:22 +0300

    added references to BLL_Tests and example how to use MOQ

--- SpaceZD.BusinessLayer.Tests/UnitTest1.cs
+++ SpaceZD.BusinessLayer.Tests/UnitTest1.cs
@@ -1,15 +1,39 @@
+using Moq;
 using NUnit.Framework;
+using SpaceZD.DataLayer.Entities;
+using SpaceZD.DataLayer.Interfaces;
 
 namespace SpaceZD.BusinessLayer.Tests;
 
 public class Tests
 {
+
+    // FOR SHOW HOW TO USE MOQ LIBRARY
+
+    private readonly Mock<IRepositorySoftDelete<User>> _userRepositoryMock;
+    public Tests()
+    {
+        _userRepositoryMock = new Mock<IRepositorySoftDelete<User>>();
+    }
+
     [SetUp]
-    public void Setup() {}
+    public void Setup() 
+    {
+    }
 
     [Test]
-    public void Test1()
+    public void UserAddTest()
     {
+
+        //_userRepositoryMock.Setup(x => x.Add(It.IsAny<User>())).Returns(23);
+        //var userService = new UserService(_userRepositoryMock.Object, new Mapper(new MapperConfiguration(cfg =>
+        //{
+        //    cfg.AddProfile<BusinessLayerMapper>();
+        //})));
+        
+
+        // 
+
         Assert.Pass();
     }
 }
