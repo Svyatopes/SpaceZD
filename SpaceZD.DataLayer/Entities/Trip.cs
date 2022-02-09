217fc6e483736a958139b56f1965482b04ab8bea
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-09 19:53:27 +0300

    Added equals to trip

--- SpaceZD.DataLayer/Entities/Trip.cs
+++ SpaceZD.DataLayer/Entities/Trip.cs
@@ -9,4 +9,19 @@ public class Trip
     public DateTime StartTime { get; set; }
     public virtual ICollection<Order> Orders { get; set; }
     public bool IsDeleted { get; set; }
+
+
+    private bool Equals(Trip other)
+    {
+        return Train.Id == other.Train.Id && Route.Id == other.Route.Id && IsDeleted == other.IsDeleted;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        return obj.GetType() == GetType() && Equals((Trip)obj);
+    }
 }
