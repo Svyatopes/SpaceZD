24a086c6272e6f6e8ccaa2feae0ef7e8373b9098
saenkaaa <saenkaaa.riki@gmail.com>
2022-02-12 20:17:46 +0300

    Added test for userService

--- SpaceZD.BusinessLayer/Models/UserModel.cs
+++ SpaceZD.BusinessLayer/Models/UserModel.cs
@@ -11,5 +11,20 @@ namespace SpaceZD.BusinessLayer.Models
         public List<OrderModel> Orders { get; set; }
         public Role Role { get; set; }
         public bool IsDeleted { get; set; }
+
+        private bool Equals(UserModel other)
+        {
+            return Name == other.Name && Login == other.Login && IsDeleted == other.IsDeleted;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            return obj.GetType() == this.GetType() && Equals((UserModel)obj);
+        }
+
     }
+    
 }
