4cb3ad686e17976c2c6a27207f94d84fbce84dfc
Stepan <cntgf.2010@yandex.ru>
2022-02-13 23:58:21 +0300

    Add Equals

--- SpaceZD.BusinessLayer/Models/RouteTransitModel.cs
+++ SpaceZD.BusinessLayer/Models/RouteTransitModel.cs
@@ -7,5 +7,21 @@
         public TimeSpan DepartingTime { get; set; }
         public TimeSpan ArrivalTime { get; set; }
         public bool IsDeleted { get; set; }
+        
+        private bool Equals(RouteTransitModel other)
+        {
+            return Transit.Equals(other.Transit) &&
+                DepartingTime.Equals(other.DepartingTime) &&
+                ArrivalTime.Equals(other.ArrivalTime) &&
+                IsDeleted == other.IsDeleted;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            return obj.GetType() == GetType() && Equals((RouteTransitModel)obj);
+        }
     }
 }
