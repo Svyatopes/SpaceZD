277649af527c06e5870cabe5d31727f3ae75d86d
Stepan <cntgf.2010@yandex.ru>
2022-02-19 19:19:19 +0300

    Refactor TripStationService

--- SpaceZD.BusinessLayer/Services/TripStationService.cs
+++ SpaceZD.BusinessLayer/Services/TripStationService.cs
@@ -7,7 +7,7 @@ namespace SpaceZD.BusinessLayer.Services;
 
 public class TripStationService : BaseService, ITripStationService
 {
-    private const int SeparationArrivalDepartingTrainMinutes = 15;
+    private const int _separationArrivalDepartingTrainMinutes = 15;
 
     private readonly ITripStationRepository _repository;
     private readonly IStationRepository _stationRepository;
@@ -34,13 +34,7 @@ public class TripStationService : BaseService, ITripStationService
         var entity = _repository.GetById(id);
         ThrowIfEntityNotFound(entity, id);
 
-        if (!_stationRepository
-            .GetReadyPlatformsStation(entity!.Station,
-                 model.ArrivalTime!.Value.AddMinutes(-SeparationArrivalDepartingTrainMinutes),
-                 model.DepartingTime!.Value.AddMinutes(SeparationArrivalDepartingTrainMinutes))
-            .Select(g => g.Id)
-            .Contains(model.Platform!.Id))
-            throw new InvalidOperationException($"Невозможно выбрать платформу c Id = {model.Platform.Id}");
+        CheckTheAbilityToStopOnTheSelectedPlatform(entity!.Station, model.ArrivalTime!.Value, model.DepartingTime!.Value, model.Platform!.Id);
 
         var platform = _platformRepository.GetById(model.Platform.Id);
         var platformModel = _mapper.Map<PlatformModel>(platform);
@@ -56,7 +50,18 @@ public class TripStationService : BaseService, ITripStationService
         ThrowIfEntityNotFound(entity, id);
 
         return _mapper.Map<List<PlatformModel>>(_stationRepository.GetReadyPlatformsStation(entity!.Station,
-            entity.ArrivalTime!.Value.AddMinutes(-SeparationArrivalDepartingTrainMinutes),
-            entity.DepartingTime!.Value.AddMinutes(SeparationArrivalDepartingTrainMinutes)));
+            entity.ArrivalTime!.Value.AddMinutes(-_separationArrivalDepartingTrainMinutes),
+            entity.DepartingTime!.Value.AddMinutes(_separationArrivalDepartingTrainMinutes)));
+    }
+
+    private void CheckTheAbilityToStopOnTheSelectedPlatform(Station station, DateTime arrivalTime, DateTime departureTime, int platformId)
+    {
+        if (!_stationRepository
+            .GetReadyPlatformsStation(station,
+                 arrivalTime.AddMinutes(-_separationArrivalDepartingTrainMinutes),
+                 departureTime.AddMinutes(_separationArrivalDepartingTrainMinutes))
+            .Select(g => g.Id)
+            .Contains(platformId))
+            throw new InvalidOperationException($"Невозможно выбрать платформу c Id = {platformId}");
     }
 }
