e73f65e3ebf94b13b49b1acc30b2de6afced38b1
saenkaaa <saenkaaa.riki@gmail.com>
2022-02-22 14:54:22 +0300

    Add validation in ticket

--- SpaceZD.API/Models/Inputs/TicketCreateInputModel.cs
+++ SpaceZD.API/Models/Inputs/TicketCreateInputModel.cs
@@ -1,9 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace SpaceZD.API.Models;
 
 public class TicketCreateInputModel : TicketUpdateInputModel
 {
-    public int OrderId { get; set; }
+    [Required]
+    public int OrderId { get; set; }  
+    
     public bool IsTeaIncluded { get; set; }
     public bool IsPetPlaceIncluded { get; set; }
 
--- SpaceZD.API/Models/Inputs/TicketUpdateInputModel.cs
+++ SpaceZD.API/Models/Inputs/TicketUpdateInputModel.cs
@@ -1,9 +1,17 @@
-﻿namespace SpaceZD.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaceZD.API.Models
 {
     public class TicketUpdateInputModel
     {
+        [Required]
         public int CarriageId { get; set; }
-        public int SeatNumber { get; set; }        
+
+        [Required]
+        [Range(1, 1000)]
+        public int SeatNumber { get; set; }
+
+        [Required]
         public int PersonId { get; set; }
     }
 }
