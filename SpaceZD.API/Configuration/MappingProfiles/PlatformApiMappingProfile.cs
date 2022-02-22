fb66f95b7775d7f9320c1b46646258e4fa50179d
svyatopes <svyatopes@gmail.com>
2022-02-22 04:02:51 +0300

    refactor platforms controller + mapping profile

--- SpaceZD.API/Configuration/MappingProfiles/PlatformApiMappingProfile.cs
+++ SpaceZD.API/Configuration/MappingProfiles/PlatformApiMappingProfile.cs
@@ -8,10 +8,15 @@ public class PlatformApiMappingProfile : Profile
 {
     public PlatformApiMappingProfile()
     {
-        CreateMap<PlatformInputModel, PlatformModel>()
+        CreateMap<PlatformInputModel, PlatformModel>();
+
+        CreateMap<PlatformAddInputModel, PlatformModel>()
             .ForMember(pm => pm.Station, opt => opt.MapFrom(pim => new StationModel { Id = pim.StationId }));
 
         CreateMap<PlatformModel, PlatformOutputModel>()
+            .ForMember(pom => pom.StationId, opt => opt.MapFrom(pm => pm.Station.Id));
+
+        CreateMap<PlatformModel, PlatformWithDeletedOutputModel>()
             .ForMember(pom => pom.StationId, opt => opt.MapFrom(pm => pm.Station.Id));
     }
 }
